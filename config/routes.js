cda0cc21827425bfe056127d009f494ad5aa62ab
Isaac <isaacdavid797@gmail.com>
2022-10-23 22:55:24 -0300

    Isolated implementation of user entity methods

--- config/routes.js
+++ config/routes.js
@@ -4,4 +4,10 @@ module.exports = app => {
 
   app.route('/users')
     .get(app.api.user.get)
+    .post(app.api.user.save)
+
+  app.route('/users/:id')
+    .put(app.api.user.save)
+    .get(app.api.user.getById)
+    .delete(app.api.user.remove)
 }
