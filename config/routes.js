44d1a4e483130a929022c056b3b527773ab87124
Isaac <isaacdavid797@gmail.com>
2022-10-25 00:08:52 -0300

    Category entity methods, implemented

--- config/routes.js
+++ config/routes.js
@@ -1,7 +1,5 @@
 module.exports = app => {
 
-
-
   app.route('/users')
     .get(app.api.user.get)
     .post(app.api.user.save)
@@ -10,4 +8,13 @@ module.exports = app => {
     .put(app.api.user.save)
     .get(app.api.user.getById)
     .delete(app.api.user.remove)
+
+  app.route('/categories')
+    .get(app.api.category.get)
+    .post(app.api.category.save)
+
+  app.route('/categories/:id')
+    .get(app.api.category.getById)
+    .put(app.api.category.save)
+    .delete(app.api.category.remove)
 }
